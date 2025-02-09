KDE/kwin

plugins/zoom: Allow configuring pointer axis modifiers
This makes the pointer axis gesture more discoverable and it can be
more convenient to zoom with one modifier key pressed than two.

src/plugins/zoom/zoom.cpp
@@ -46,12 +46,10 @@ ZoomEffect::ZoomEffect()
     a = KStandardAction::zoomIn(this, &ZoomEffect::zoomIn, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus) << (Qt::META | Qt::Key_Equal));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus) << (Qt::META | Qt::Key_Equal));
-    effects->registerAxisShortcut(Qt::ControlModifier | Qt::MetaModifier, PointerAxisDown, a);
 
     a = KStandardAction::zoomOut(this, &ZoomEffect::zoomOut, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Minus));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Minus));
-    effects->registerAxisShortcut(Qt::ControlModifier | Qt::MetaModifier, PointerAxisUp, a);
 
     a = KStandardAction::actualSize(this, &ZoomEffect::actualSize, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_0));
@@ -206,6 +204,29 @@ void ZoomEffect::hideCursor()
     }
 }
 
+static Qt::KeyboardModifiers stringToKeyboardModifiers(const QString &string)
+{
+    const QStringList parts = string.split(QLatin1Char('+'));
+    if (parts.isEmpty()) {
+        return Qt::KeyboardModifiers();
+    }
+
+    Qt::KeyboardModifiers modifiers;
+    for (const QString &part : parts) {
+        if (part == QLatin1String("Meta")) {
+            modifiers |= Qt::MetaModifier;
+        } else if (part == QLatin1String("Ctrl")) {
+            modifiers |= Qt::ControlModifier;
+        } else if (part == QLatin1String("Alt")) {
+            modifiers |= Qt::AltModifier;
+        } else if (part == QLatin1String("Shift")) {
+            modifiers |= Qt::ShiftModifier;
+        }
+    }
+
+    return modifiers;
+}
+
 void ZoomEffect::reconfigure(ReconfigureFlags)
 {
     ZoomConfig::self()->read();
@@ -228,6 +249,23 @@ void ZoomEffect::reconfigure(ReconfigureFlags)
     m_focusDelay = std::max(uint(0), ZoomConfig::focusDelay());
     // The factor the zoom-area will be moved on touching an edge on push-mode or using the navigation KAction's.
     m_moveFactor = std::max(0.1, ZoomConfig::moveFactor());
+
+    const Qt::KeyboardModifiers pointerAxisModifiers = stringToKeyboardModifiers(ZoomConfig::pointerAxisGestureModifiers());
+    if (m_axisModifiers != pointerAxisModifiers) {
+        m_zoomInAxisAction.reset();
+        m_zoomOutAxisAction.reset();
+        m_axisModifiers = pointerAxisModifiers;
+
+        if (pointerAxisModifiers) {
+            m_zoomInAxisAction = std::make_unique<QAction>();
+            connect(m_zoomInAxisAction.get(), &QAction::triggered, this, &ZoomEffect::zoomIn);
+            effects->registerAxisShortcut(pointerAxisModifiers, PointerAxisDown, m_zoomInAxisAction.get());
+
+            m_zoomOutAxisAction = std::make_unique<QAction>();
+            connect(m_zoomOutAxisAction.get(), &QAction::triggered, this, &ZoomEffect::zoomOut);
+            effects->registerAxisShortcut(pointerAxisModifiers, PointerAxisUp, m_zoomOutAxisAction.get());
+        }
+    }
 }
 
 void ZoomEffect::prePaintScreen(ScreenPrePaintData &data, std::chrono::milliseconds presentTime)

src/plugins/zoom/zoom_config.cpp
@@ -21,6 +21,7 @@
 #include <KGlobalAccel>
 #include <KLocalizedString>
 #include <KPluginFactory>
+#include <KWindowSystem>
 
 #include <QVBoxLayout>
 
@@ -28,13 +29,109 @@ K_PLUGIN_CLASS(KWin::ZoomEffectConfig)
 
 namespace KWin
 {
+
+static QString encodeModifierOnlySequence(const QKeySequence &sequence)
+{
+    if (sequence.isEmpty()) {
+        return QString();
+    }
+
+    // Normalize from modifiers+key to modifiers
+    Qt::KeyboardModifiers modifiers = sequence[0].keyboardModifiers();
+    switch (sequence[0].key()) {
+    case Qt::Key_Meta:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+        modifiers |= Qt::MetaModifier;
+        break;
+    case Qt::Key_Shift:
+        modifiers |= Qt::ShiftModifier;
+        break;
+    case Qt::Key_Control:
+        modifiers |= Qt::ControlModifier;
+        break;
+    case Qt::Key_Alt:
+        modifiers |= Qt::AltModifier;
+        break;
+    default:
+        break;
+    }
+
+    QStringList encoded;
+    if (modifiers & Qt::MetaModifier) {
+        encoded += QLatin1String("Meta");
+    }
+    if (modifiers & Qt::ControlModifier) {
+        encoded += QLatin1String("Ctrl");
+    }
+    if (modifiers & Qt::AltModifier) {
+        encoded += QLatin1String("Alt");
+    }
+    if (modifiers & Qt::ShiftModifier) {
+        encoded += QLatin1String("Shift");
+    }
+
+    return encoded.join(QLatin1Char('+'));
+}
+
+static QKeySequence decodeModifierOnlySequence(const QString &string)
+{
+    const QStringList parts = string.split(QLatin1Char('+'));
+    if (parts.isEmpty()) {
+        return QKeySequence();
+    }
+
+    Qt::KeyboardModifiers modifiers;
+    for (const QString &part : parts) {
+        if (part == QLatin1String("Meta")) {
+            modifiers |= Qt::MetaModifier;
+        } else if (part == QLatin1String("Ctrl")) {
+            modifiers |= Qt::ControlModifier;
+        } else if (part == QLatin1String("Alt")) {
+            modifiers |= Qt::AltModifier;
+        } else if (part == QLatin1String("Shift")) {
+            modifiers |= Qt::ShiftModifier;
+        }
+    }
+
+    // Normalize the key sequence to modifiers+key. QKeySequence doesn't understand sequences that contain only modifiers.
+    if (modifiers & Qt::ShiftModifier) {
+        return Qt::Key_Shift | (modifiers & ~Qt::ShiftModifier);
+    } else if (modifiers & Qt::AltModifier) {
+        return Qt::Key_Alt | (modifiers & ~Qt::AltModifier);
+    } else if (modifiers & Qt::ControlModifier) {
+        return Qt::Key_Control | (modifiers & ~Qt::ControlModifier);
+    } else if (modifiers & Qt::MetaModifier) {
+        return Qt::Key_Meta | (modifiers & ~Qt::MetaModifier);
+    } else {
+        return QKeySequence();
+    }
+}
+
+PointerAxisGestureModifiersWidget::PointerAxisGestureModifiersWidget(QWidget *parent)
+    : KKeySequenceWidget(parent)
+{
+    setProperty("kcfg_property", QStringLiteral("modifiers"));
+    setCheckForConflictsAgainst(None);
+    setModifierOnlyAllowed(true);
+    connect(this, &KKeySequenceWidget::keySequenceChanged, this, &PointerAxisGestureModifiersWidget::modifiersChanged);
+}
+
+QString PointerAxisGestureModifiersWidget::modifiers() const
+{
+    return encodeModifierOnlySequence(keySequence());
+}
+
+void PointerAxisGestureModifiersWidget::setModifiers(const QString &modifiers)
+{
+    setKeySequence(decodeModifierOnlySequence(modifiers));
+}
+
 ZoomEffectConfig::ZoomEffectConfig(QObject *parent, const KPluginMetaData &data)
     : KCModule(parent, data)
 {
     ZoomConfig::instance(KWIN_CONFIG);
     m_ui.setupUi(widget());
-
-    addConfig(ZoomConfig::self(), widget());
 
     connect(m_ui.editor, &KShortcutsEditor::keyChange, this, &KCModule::markAsChanged);
 
@@ -107,7 +204,16 @@ ZoomEffectConfig::ZoomEffectConfig(QObject *parent, const KPluginMetaData &data)
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_F6));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_F6));
 
+    if (KWindowSystem::isPlatformWayland()) {
+        auto axisGesture = new PointerAxisGestureModifiersWidget();
+        axisGesture->setObjectName(QLatin1String("kcfg_PointerAxisGestureModifiers"));
+        m_ui.formLayout->addRow(new QLabel(i18n("Wheel gesture modifier keys:")), axisGesture);
+        m_ui.formLayout->addRow(nullptr, new QLabel(i18n("Use the mouse wheel while the modifier keys are pressed to zoom")));
+    }
+
     m_ui.editor->addCollection(actionCollection);
+
+    addConfig(ZoomConfig::self(), widget());
 }
 
 void ZoomEffectConfig::save()

josephcrowell/kwin-x11-improved

src/plugins/zoom/zoom.cpp
@@ -46,12 +46,10 @@ ZoomEffect::ZoomEffect()
     a = KStandardAction::zoomIn(this, &ZoomEffect::zoomIn, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus) << (Qt::META | Qt::Key_Equal));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus) << (Qt::META | Qt::Key_Equal));
-    effects->registerAxisShortcut(Qt::ControlModifier | Qt::MetaModifier, PointerAxisUp, a);
 
     a = KStandardAction::zoomOut(this, &ZoomEffect::zoomOut, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Minus));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Minus));
-    effects->registerAxisShortcut(Qt::ControlModifier | Qt::MetaModifier, PointerAxisDown, a);
 
     a = KStandardAction::actualSize(this, &ZoomEffect::actualSize, this);
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_0));
@@ -206,6 +204,29 @@ void ZoomEffect::hideCursor()
     }
 }
 
+static Qt::KeyboardModifiers stringToKeyboardModifiers(const QString &string)
+{
+    const QStringList parts = string.split(QLatin1Char('+'));
+    if (parts.isEmpty()) {
+        return Qt::KeyboardModifiers();
+    }
+
+    Qt::KeyboardModifiers modifiers;
+    for (const QString &part : parts) {
+        if (part == QLatin1String("Meta")) {
+            modifiers |= Qt::MetaModifier;
+        } else if (part == QLatin1String("Ctrl")) {
+            modifiers |= Qt::ControlModifier;
+        } else if (part == QLatin1String("Alt")) {
+            modifiers |= Qt::AltModifier;
+        } else if (part == QLatin1String("Shift")) {
+            modifiers |= Qt::ShiftModifier;
+        }
+    }
+
+    return modifiers;
+}
+
 void ZoomEffect::reconfigure(ReconfigureFlags)
 {
     ZoomConfig::self()->read();
@@ -228,6 +249,23 @@ void ZoomEffect::reconfigure(ReconfigureFlags)
     m_focusDelay = std::max(uint(0), ZoomConfig::focusDelay());
     // The factor the zoom-area will be moved on touching an edge on push-mode or using the navigation KAction's.
     m_moveFactor = std::max(0.1, ZoomConfig::moveFactor());
+
+    const Qt::KeyboardModifiers pointerAxisModifiers = stringToKeyboardModifiers(ZoomConfig::pointerAxisGestureModifiers());
+    if (m_axisModifiers != pointerAxisModifiers) {
+        m_zoomInAxisAction.reset();
+        m_zoomOutAxisAction.reset();
+        m_axisModifiers = pointerAxisModifiers;
+
+        if (pointerAxisModifiers) {
+            m_zoomInAxisAction = std::make_unique<QAction>();
+            connect(m_zoomInAxisAction.get(), &QAction::triggered, this, &ZoomEffect::zoomIn);
+            effects->registerAxisShortcut(pointerAxisModifiers, PointerAxisUp, m_zoomInAxisAction.get());
+
+            m_zoomOutAxisAction = std::make_unique<QAction>();
+            connect(m_zoomOutAxisAction.get(), &QAction::triggered, this, &ZoomEffect::zoomOut);
+            effects->registerAxisShortcut(pointerAxisModifiers, PointerAxisDown, m_zoomOutAxisAction.get());
+        }
+    }
 }
 
 void ZoomEffect::prePaintScreen(ScreenPrePaintData &data, std::chrono::milliseconds presentTime)

src/plugins/zoom/zoom_config.cpp
@@ -15,26 +15,123 @@
 #include "zoomconfig.h"
 #include <kwineffects_interface.h>
 
-#include <QAction>
-
 #include <KActionCollection>
 #include <KGlobalAccel>
 #include <KLocalizedString>
 #include <KPluginFactory>
+#include <KWindowSystem>
 
+#include <QAction>
+#include <QFontDatabase>
 #include <QVBoxLayout>
 
 K_PLUGIN_CLASS(KWin::ZoomEffectConfig)
 
 namespace KWin
 {
+
+static QString encodeModifierOnlySequence(const QKeySequence &sequence)
+{
+    if (sequence.isEmpty()) {
+        return QString();
+    }
+
+    // Normalize from modifiers+key to modifiers
+    Qt::KeyboardModifiers modifiers = sequence[0].keyboardModifiers();
+    switch (sequence[0].key()) {
+    case Qt::Key_Meta:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+        modifiers |= Qt::MetaModifier;
+        break;
+    case Qt::Key_Shift:
+        modifiers |= Qt::ShiftModifier;
+        break;
+    case Qt::Key_Control:
+        modifiers |= Qt::ControlModifier;
+        break;
+    case Qt::Key_Alt:
+        modifiers |= Qt::AltModifier;
+        break;
+    default:
+        break;
+    }
+
+    QStringList encoded;
+    if (modifiers & Qt::MetaModifier) {
+        encoded += QLatin1String("Meta");
+    }
+    if (modifiers & Qt::ControlModifier) {
+        encoded += QLatin1String("Ctrl");
+    }
+    if (modifiers & Qt::AltModifier) {
+        encoded += QLatin1String("Alt");
+    }
+    if (modifiers & Qt::ShiftModifier) {
+        encoded += QLatin1String("Shift");
+    }
+
+    return encoded.join(QLatin1Char('+'));
+}
+
+static QKeySequence decodeModifierOnlySequence(const QString &string)
+{
+    const QStringList parts = string.split(QLatin1Char('+'));
+    if (parts.isEmpty()) {
+        return QKeySequence();
+    }
+
+    Qt::KeyboardModifiers modifiers;
+    for (const QString &part : parts) {
+        if (part == QLatin1String("Meta")) {
+            modifiers |= Qt::MetaModifier;
+        } else if (part == QLatin1String("Ctrl")) {
+            modifiers |= Qt::ControlModifier;
+        } else if (part == QLatin1String("Alt")) {
+            modifiers |= Qt::AltModifier;
+        } else if (part == QLatin1String("Shift")) {
+            modifiers |= Qt::ShiftModifier;
+        }
+    }
+
+    // Normalize the key sequence to modifiers+key. QKeySequence doesn't understand sequences that contain only modifiers.
+    if (modifiers & Qt::ShiftModifier) {
+        return Qt::Key_Shift | (modifiers & ~Qt::ShiftModifier);
+    } else if (modifiers & Qt::AltModifier) {
+        return Qt::Key_Alt | (modifiers & ~Qt::AltModifier);
+    } else if (modifiers & Qt::ControlModifier) {
+        return Qt::Key_Control | (modifiers & ~Qt::ControlModifier);
+    } else if (modifiers & Qt::MetaModifier) {
+        return Qt::Key_Meta | (modifiers & ~Qt::MetaModifier);
+    } else {
+        return QKeySequence();
+    }
+}
+
+PointerAxisGestureModifiersWidget::PointerAxisGestureModifiersWidget(QWidget *parent)
+    : KKeySequenceWidget(parent)
+{
+    setProperty("kcfg_property", QStringLiteral("modifiers"));
+    setCheckForConflictsAgainst(None);
+    setPatterns(KKeySequenceRecorder::Pattern::Modifier);
+    connect(this, &KKeySequenceWidget::keySequenceChanged, this, &PointerAxisGestureModifiersWidget::modifiersChanged);
+}
+
+QString PointerAxisGestureModifiersWidget::modifiers() const
+{
+    return encodeModifierOnlySequence(keySequence());
+}
+
+void PointerAxisGestureModifiersWidget::setModifiers(const QString &modifiers)
+{
+    setKeySequence(decodeModifierOnlySequence(modifiers));
+}
+
 ZoomEffectConfig::ZoomEffectConfig(QObject *parent, const KPluginMetaData &data)
     : KCModule(parent, data)
 {
     ZoomConfig::instance(KWIN_CONFIG);
     m_ui.setupUi(widget());
-
-    addConfig(ZoomConfig::self(), widget());
 
     connect(m_ui.editor, &KShortcutsEditor::keyChange, this, &KCModule::markAsChanged);
 
@@ -107,7 +204,19 @@ ZoomEffectConfig::ZoomEffectConfig(QObject *parent, const KPluginMetaData &data)
     KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_F6));
     KGlobalAccel::self()->setShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_F6));
 
+    if (KWindowSystem::isPlatformWayland()) {
+        auto axisGesture = new PointerAxisGestureModifiersWidget();
+        axisGesture->setObjectName(QLatin1String("kcfg_PointerAxisGestureModifiers"));
+        m_ui.formLayout->addRow(new QLabel(i18n("Scroll gesture modifier keys:")), axisGesture);
+
+        auto description = new QLabel(i18n("Scroll while the modifier keys are pressed to zoom"));
+        description->setFont(QFontDatabase::systemFont(QFontDatabase::SmallestReadableFont));
+        m_ui.formLayout->addRow(nullptr, description);
+    }
+
     m_ui.editor->addCollection(actionCollection);
+
+    addConfig(ZoomConfig::self(), widget());
 }
 
 void ZoomEffectConfig::save()